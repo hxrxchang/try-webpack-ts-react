174a8994d662629af036927026b4e08bdfeb863e
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-06-23 21:47:08 +0900

    ts setting

--- src/a.ts
+++ src/a.ts
@@ -1,4 +1,6 @@
 const rootElement = document.getElementById("root");
 export default function a() {
-  rootElement.innerHTML = "JS is injected!!!";
+  if (rootElement) {
+    rootElement.innerHTML = "JS is injected!!!";
+  }
 }
