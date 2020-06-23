3c1cf6b65f0de7eee1ef83f801845bf109459a86
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-06-23 21:42:53 +0900

    dev server

--- src/a.js
+++ src/a.js
@@ -1,1 +1,4 @@
-export default () => console.log("function callled");
+const rootElement = document.getElementById("root");
+export default function a() {
+  rootElement.innerHTML = "JS is injected!!!";
+}
